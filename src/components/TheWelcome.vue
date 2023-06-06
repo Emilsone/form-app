c7a1fc46ec2336e4c5df95b4d7cf326e7f10bb6b
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-06 17:57:25 +0100

    fix file

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,21 +5,26 @@
 <template>
   <main>
     <form name="contact" netlify method="POST" data-netlify="true">
-      <p>
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" />
-      </p>
-      <p>
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email" />
-      </p>
-      <p>
-        <label for="message">Message</label>
-        <textarea id="message" name="message"></textarea>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+      <div class="relative mb-4">
+        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+        <input type="text" id="name" name="name"
+          class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email"
+          class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div class="relative mb-4">
+        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+        <textarea id="message" name="message"
+          class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      </div>
+      <button
+        class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+      <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook
+        viral
+        artisan.</p>
     </form>
   </main>
 </template>
