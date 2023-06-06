2ae951c25a2c3b992913a26cbb5126e374e37222
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-06 17:47:45 +0100

    create from layout

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <form name="contact" netlify method="POST" data-netlify="true">
+      <p>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" />
+      </p>
+      <p>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" />
+      </p>
+      <p>
+        <label for="message">Message</label>
+        <textarea id="message" name="message"></textarea>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </main>
+</template>
