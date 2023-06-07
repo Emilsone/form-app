9bfe9642af7e8004865a00cba7e80927d6218182
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-07 15:42:43 +0100

    add input

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,6 +5,7 @@
 <template>
   <main>
     <form name="contact" netlify method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact" />
       <div class="relative mb-4">
         <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
         <input type="text" id="name" name="name"
@@ -21,7 +22,7 @@
           class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
       </div>
       <button
-        class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+        class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
       <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook
         viral
         artisan.</p>
