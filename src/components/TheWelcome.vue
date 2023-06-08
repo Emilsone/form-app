d1164c024824e8519270124404ba4faaa21ca992
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-08 21:11:10 +0100

    fix form data

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,17 +1,29 @@
-
-<template>
-  <main>
-    <form name="Contact" method="POST" netlify data-netlify="true" data-netlify-honeypot="bot-field">
-  <input type="hidden" name="form-name" value="Contact" />
-    <input type="text" name="name" placeholder="Name"/>   
-    <input type="email" name="email" placeholder="Email"/>
-    <textarea name="message" placeholder="Message"></textarea>
-    <div class="form-btn"><button type="submit">SUBMIT</button></div>
-</form>
-  </main>
-</template>
-
 <script setup>
 
 </script>
 
+<template>
+  <main>
+    <form name="contact" method="POST" class="contact__form" netlify-honeypot="bot-field" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact" />
+      <div class="relative mb-4">
+        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+        <input type="text" id="name" name="name"
+          class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email"
+          class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div class="relative mb-4">
+        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+        <textarea id="message" name="message"
+          class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      </div>
+      <button
+        class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
+
+    </form>
+  </main>
+</template>
